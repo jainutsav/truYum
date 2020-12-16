12c5585ade7b76f3d2dee551fdeec347db2d6698
jainutsav <47519014+jainutsav@users.noreply.github.com>
2020-12-16 11:19:59 +0530

    Add files via upload

---
+++ Java/src/com/cognizant/truyum/dao/CartEmptyException.java
@@ -0,0 +1,20 @@
+package com.cognizant.truyum.dao;
+
+@SuppressWarnings("serial")
+public class CartEmptyException extends Exception {
+	private String message;
+
+	public CartEmptyException(String message) {
+		super(message);
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
+//*UJ*
