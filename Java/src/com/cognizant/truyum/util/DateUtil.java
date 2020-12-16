33477fcddf72fa370c0f6628ab9c2bfde7f4a21d
jainutsav <47519014+jainutsav@users.noreply.github.com>
2020-12-16 11:22:07 +0530

    Add files via upload

---
+++ Java/src/com/cognizant/truyum/util/DateUtil.java
@@ -0,0 +1,13 @@
+package com.cognizant.truyum.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+	public static Date convertToDate(String dateToBeConverted)
+			throws ParseException {
+		return new SimpleDateFormat("dd/mm/yyyy").parse(dateToBeConverted);
+	}
+}
+//*UJ*
